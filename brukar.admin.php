b7a20a1ae4a7f0f81d8fd3b63bce3b6ef19ec20c
Erlend Klakegg Bergheim <erlend@klakegg.net>
2012-04-22 00:34:13 +0200

    * Split oauth and admin into two files.

---
+++ brukar.admin.php
@@ -0,0 +1,68 @@
+<?php
+
+function brukar_admin($form, &$form_state = array()) {
+	$form['server'] = array(
+	  '#type' => 'fieldset',
+	  '#title' => 'Server',
+	  '#collapsible' => variable_get('brukar_url', '') != '',
+	  '#collapsed' => variable_get('brukar_url', '') != '',
+	);
+  $form['server']['brukar_url'] = array(
+    '#type' => 'textfield',
+    '#title' => 'Adresse',
+    '#default_value' => variable_get('brukar_url', ''),
+  );
+  $form['server']['brukar_consumer_key'] = array(
+    '#type' => 'textfield',
+    '#title' => 'Consumer key',
+    '#default_value' => variable_get('brukar_consumer_key', ''),
+  );
+  $form['server']['brukar_consumer_secret'] = array(
+    '#type' => 'textfield',
+    '#title' => 'Consumer secret',
+    '#default_value' => variable_get('brukar_consumer_secret', ''),
+  );
+  $form['behavior'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'Behavior',
+  );
+  $form['behavior']['brukar_keyword'] = array(
+    '#type' => 'textfield',
+    '#title' => 'Keyword',
+    '#default_value' => variable_get('brukar_keyword', 'brukar'),
+  );
+  $form['behavior']['brukar_name'] = array(
+    '#type' => 'select',
+    '#title' => 'Name in database',
+    '#options' => array(
+      '!name' => '[Name]',
+      '!name (!sident)' => '[Name] ([Short ident])',
+      '!ident' => '[Ident]',
+    ),
+    '#default_value' => variable_get('brukar_name', '!name'),
+  );
+  $form['behavior']['brukar_forced'] = array(
+    '#type' => 'radios',
+    '#title' => 'Forced login',
+    '#options' => array('Nei', 'Ja'),
+    '#default_value' => variable_get('brukar_forced', '0'),
+  );
+
+  return system_settings_form($form);
+}
+
+function brukar_admin_validate($form, &$form_state) {
+  require_once(drupal_get_path('module', 'brukar') . '/OAuth.php');
+
+  $method = new OAuthSignatureMethod_HMAC_SHA1();
+  $consumer = new OAuthConsumer($form_state['values']['brukar_consumer_key'], $form_state['values']['brukar_consumer_secret']);
+  $callback =  url($_GET['q'], array('absolute' => TRUE));
+
+  $req = OAuthRequest::from_consumer_and_token($consumer, NULL, 'GET', $form_state['values']['brukar_url'] . 'server/oauth/request_token', array('oauth_callback' => $callback));
+  $req->sign_request($method, $consumer, NULL);
+  parse_str(trim(@file_get_contents($req->to_url())), $token);
+
+  if (count($token) == 0) {
+    form_set_error('server', t('Invalid settings.'));
+  }
+}
--- brukar.oauth.php
+++ brukar.oauth.php
@@ -1,73 +1,8 @@
 <?php
 
-function brukar_admin($form, &$form_state = array()) {
-	$form['server'] = array(
-	  '#type' => 'fieldset',
-	  '#title' => 'Server',
-	  '#collapsible' => variable_get('brukar_url', '') != '',
-	  '#collapsed' => variable_get('brukar_url', '') != '',
-	);
-  $form['server']['brukar_url'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Adresse',
-    '#default_value' => variable_get('brukar_url', ''),
-  );
-  $form['server']['brukar_consumer_key'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Consumer key',
-    '#default_value' => variable_get('brukar_consumer_key', ''),
-  );
-  $form['server']['brukar_consumer_secret'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Consumer secret',
-    '#default_value' => variable_get('brukar_consumer_secret', ''),
-  );
-  $form['behavior'] = array(
-    '#type' => 'fieldset',
-    '#title' => 'Behavior',
-  );
-  $form['behavior']['brukar_keyword'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Keyword',
-    '#default_value' => variable_get('brukar_keyword', 'brukar'),
-  );
-  $form['behavior']['brukar_name'] = array(
-    '#type' => 'select',
-    '#title' => 'Name in database',
-    '#options' => array(
-      '!name' => '[Name]',
-      '!name (!sident)' => '[Name] ([Short ident])',
-      '!ident' => '[Ident]',
-    ),
-    '#default_value' => variable_get('brukar_name', '!name'),
-  );
-  $form['behavior']['brukar_forced'] = array(
-    '#type' => 'radios',
-    '#title' => 'Forced login',
-    '#options' => array('Nei', 'Ja'),
-    '#default_value' => variable_get('brukar_forced', '0'),
-  );
-
-  return system_settings_form($form);
-}
-
-function brukar_admin_validate($form, &$form_state) {
+function brukar_oauth_request() {
   require_once(drupal_get_path('module', 'brukar') . '/OAuth.php');
 
-  $method = new OAuthSignatureMethod_HMAC_SHA1();
-  $consumer = new OAuthConsumer($form_state['values']['brukar_consumer_key'], $form_state['values']['brukar_consumer_secret']);
-  $callback =  url($_GET['q'], array('absolute' => TRUE));
-
-  $req = OAuthRequest::from_consumer_and_token($consumer, NULL, 'GET', $form_state['values']['brukar_url'] . 'server/oauth/request_token', array('oauth_callback' => $callback));
-  $req->sign_request($method, $consumer, NULL);
-  parse_str(trim(@file_get_contents($req->to_url())), $token);
-
-  if (count($token) == 0) {
-    form_set_error('server', t('Invalid settings.'));
-  }
-}
-
-function brukar_oauth_request() {
   $method = new OAuthSignatureMethod_HMAC_SHA1();
   $consumer = new OAuthConsumer(variable_get('brukar_consumer_key'), variable_get('brukar_consumer_secret'));
   $callback =  url($_GET['q'], array('absolute' => TRUE));
@@ -86,6 +21,8 @@ function brukar_oauth_request() {
 }
 
 function brukar_oauth_callback() {
+  require_once(drupal_get_path('module', 'brukar') . '/OAuth.php');
+
   $method = new OAuthSignatureMethod_HMAC_SHA1();
   $consumer = new OAuthConsumer(variable_get('brukar_consumer_key'), variable_get('brukar_consumer_secret'));
 
